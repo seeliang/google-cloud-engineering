1d07e224ac2b9acadb7c78dc8e68e159ee2479c8
sihui liang <sihui.liang@gumtreegroup.com.au>
2025-10-18 18:26:28 +1100

    chore: relocate

--- server/deploy-server.bash
+++ server/deploy-server.bash
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Deploy the analyzeText Cloud Function. Additional gcloud flags can be passed through.
 gcloud functions deploy analyzeText \
-  --runtime=nodejs18 \
+  --runtime=nodejs20 \
   --entry-point=analyzeTextHandler \
   --trigger-http \
   --allow-unauthenticated \
